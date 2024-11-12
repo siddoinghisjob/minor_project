f57e31642735f00831d0fcc8ddffb695b10b9ef9
Soumya Deep Sarkar <96379749+siddoinghisjob@users.noreply.github.com>
2024-11-13 00:33:25 +0530

    done

--- App.py
+++ App.py
@@ -81,7 +81,7 @@ def course_recommender(course_list):
     return rec_course
 
 
-connection = pymysql.connect(host=os.getenv('API_HOST'), user=os.getenv('API_USER'), password=os.getenv('API_PASS'), db=os.getenv('API_DB'), port=os.getenv('API_PORT'), ssl={'ssl': {}})
+connection = pymysql.connect(host=os.getenv('API_HOST'), user=os.getenv('API_USER'), password=os.getenv('API_PASS'), db=os.getenv('API_DB'), port=int(os.getenv('API_PORT')), ssl={'ssl': {}})
 cursor = connection.cursor()
 
 
